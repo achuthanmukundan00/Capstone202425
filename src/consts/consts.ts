5418e6a7dfc7d7aa13cd78dd62846c27799aa672
Mehdi Essoussi <messoussi@30fe.com>
2025-02-17 14:09:52 -0800

    Added arrow opacity based on electric field

--- src/consts/consts.ts
+++ src/consts/consts.ts
@@ -2,7 +2,7 @@
 export const K = 8.9875517923e9; // Coulomb constant (N·m²/C²)
 
 // Vector Field Consts
-export const FIELD_SPACING = 64; // Spacing between field vectors in pixels
+export const FIELD_SPACING = 40; // Spacing between field vectors in pixels
 export const VECTOR_LENGTH_SCALE = 0.0005; // Scaling factor for vector length
 export const ARROWHEAD_LENGTH = 8; // Length of the arrowhead
 export const MAX_VECTOR_LENGTH = 30; // Maximum length for the vectors (to prevent too long arrows)
@@ -13,4 +13,4 @@ export const MAG_FORCE_ARROW_COLOUR = 0x6832a8;
 
 // Animation
 export const ANIMATION_SPEED = 8e-2;
-export const FORCE_SCALING = 4e-5; 
+export const FORCE_SCALING = 4e-5;
