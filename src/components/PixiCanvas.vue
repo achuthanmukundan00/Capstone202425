11782bba27ffc1b17d963beefc933b9a3e5eaef9
ACHUTHAN MUKUNDAN <achuthanmukundan00@gmail.com>
2025-03-22 17:52:16 -0400

    track mouse in pixicanvas

--- src/components/PixiCanvas.vue
+++ src/components/PixiCanvas.vue
@@ -7,10 +7,13 @@ import { ref, onMounted, onBeforeUnmount, watch } from 'vue';
 import * as PIXI from 'pixi.js';
 import type { Charge } from '@/stores/charges';
 import { useChargesStore } from '@/stores/charges';
-import { drawElectricField, drawMagneticField, drawMagneticForcesOnAllCharges, drawVelocityOnAllCharges, removeFields} from '@/utils/drawingUtils';
+import { drawElectricField, drawMagneticField, drawMagneticForcesOnAllCharges, drawVelocityOnAllCharges, removeFields } from '@/utils/drawingUtils';
 import { calculateMagneticForce } from '@/utils/mathUtils';
 import { ANIMATION_SPEED, FORCE_SCALING } from '@/consts';
+import { getNetElectricFieldAtPoint } from '@/utils/mathUtils'
 
+const canvasRef = ref<HTMLDivElement | null>(null);
+const fieldReadout = ref('');
 const canvasContainer = ref<HTMLElement | null>(null);
 const chargesStore = useChargesStore();
 const chargesGraphics = new Map<string, PIXI.Graphics>(); // Map to keep track of drawn charges
@@ -19,6 +22,18 @@ let animationFrameId: number;
 let app: PIXI.Application | null = null;
 
 const MOVEMENT_STEP = 10; // pixels per keypress
+
+function handleMouseMove(event: MouseEvent) {
+  const rect = canvasRef.value?.getBoundingClientRect()
+  if (!rect) return
+
+  const x = event.clientX - rect.left
+  const y = event.clientY - rect.top
+
+  const field = getNetElectricFieldAtPoint({ x, y })
+
+  fieldReadout.value = `Ex: ${field.x.toFixed(2)}, Ey: ${field.y.toFixed(2)}`
+}
 
 const handleKeyDown = (event: KeyboardEvent) => {
   if (event.key === 'Tab') {
@@ -92,6 +107,8 @@ onMounted(async () => {
     width: window.innerWidth,
     height: window.innerHeight
   });
+
+  canvasRef.value?.addEventListener('mousemove', handleMouseMove);
 
   // Enable zIndex sorting so that graphics with higher zIndex render on top
   app.stage.sortableChildren = true;
@@ -196,7 +213,9 @@ onMounted(async () => {
 });
 
 onBeforeUnmount(() => {
+  canvasRef.value?.removeEventListener('mousemove', handleMouseMove);
   cancelAnimationFrame(animationFrameId);
+
   if (app) {
     app.destroy(true, { children: true });
     app = null;
